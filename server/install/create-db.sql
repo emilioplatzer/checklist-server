39df2366881bf4c618b9e4e96bfec6ed5f31e6fb
Emilio Platzer <emilioplatzer@gmail.com>
2015-05-25 16:01:03 -0300

    cambio los nombres de la base de datos

--- server/install/create-db.sql
+++ server/install/create-db.sql
@@ -1,11 +1,11 @@
-create user interviewer_owner password 'ivown3210098';
-create database interviewer_db with owner interviewer_owner;
-\c interviewer_db
-create schema inter authorization interviewer_owner;
-set search_path = inter, public;
+create user nuestros_clsow password 'ivown3210098';
+create database nuestros_clsdb with owner nuestros_clsow;
+\c nuestros_clsdb
+create schema comun authorization nuestros_clsow;
+set search_path = comun, public;
 create table users(
   username text,
   hashpass text
 );
-alter table users owner to interviewer_owner;
+alter table users owner to nuestros_clsow;
 insert into users(username, hashpass) values ('guest', md5('1234guest'));
